cf53ff92c624e7e03937f6a14e86cf4c3f502af8
MatePls <33403218+MatePls@users.noreply.github.com>
2017-11-07 23:59:23 +0000

    Add files via upload
    This is the very first version im uploading today on the 7/11/17

---
+++ Coursework/Payroll System/src/payroll/system/EmployeeRegistration.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payroll.system;
+import java.util.Scanner;
+/**
+ *
+ * @author w1545923
+ */
+public class EmployeeRegistration {
+        String Input[];
+
+    Scanner Input= new Scanner (System.in);
+    System.out.println("Please Enter First Name/s");
+    
+}
---
+++ Coursework/Payroll System/src/payroll/system/PayrollSystem.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payroll.system;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author w1549523
+ */
+
+
+public class PayrollSystem {
+    
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+       GetData();
+
+        
+         System.out.println("Mate it work");
+        
+    }
+    public static int GetData(){
+    Scanner GetData = new Scanner(System.in);
+    int choice;
+         
+       System.out.println(" Enter 1 to register a new employee\n Enter 2 to show Full Time Employees\n Enter 3 to show Part Time Employees");
+        choice =GetData.nextInt();
+       if (choice > 3  && choice < 0){
+       System.out.println("Mate Come on, Enter a number between 1-3");
+       }
+       switch (choice)
+        {
+            case 1: System.out.println( "You entered 1");
+            break;
+                
+            case 2: System.out.println("You Entered 2");
+            break;
+            
+            case 3: System.out.println("You entered 3");
+            break;
+            
+            default: System.out.println("Congrats you've messed up a simple number entry");
+        }
+        return choice ;
+        
+}
+}
+      
+    
+
